886e60d6af7279db56c224971304db0bdbd195f6
Thibaud Jourdain <jourdain.thibaud@gmail.com>
2020-03-12 16:37:53 +0100

    Classe objet de sortie

---
+++ .history/Out_20200312163725.py
@@ -0,0 +1,41 @@
+class Out:
+    #Obligatoire
+    #Alphanumérique
+    #Unique
+    ref_fournisseur = ""
+
+    #Obligatoire
+    #Alphanumérique
+    #Unique
+    designation = ""
+
+    #Obligatoire
+    #Numérique
+    litrage = 0
+
+    #Non obligatoire
+    #Numérique
+    conditionnement = 0
+
+    #Obligatoire
+    #Numérique
+    #Unique
+    #EAN13
+    code_barre = 0
+
+    #Obligatoire
+    #Numérique
+    prix_achat = 0
+
+    #Obligatoire
+    #Numérique
+    prix_vente = 0
+
+    #Obligatoire
+    _type = ""
+
+    #Obligatoire
+    modele = ""
+
+    #Obligatoire
+    marque = ""
